c024dd737130f94c47d07ccc450a824acbaca540
Raghav Aggiwal <aggiwalraghav@gmail.com>
2020-07-21 11:30:36 +0530

    commit

---
+++ lib/logger/logger.dart
@@ -0,0 +1,6 @@
+import 'package:logger/logger.dart';
+
+//for logging
+Logger getLogger() {
+  return Logger(printer: PrettyPrinter());
+}
---
+++ lib/networking/api_provider.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+
+import 'package:myflutterpackages/networking/custom_exception.dart';
+import 'package:myflutterpackages/logger/logger.dart';
+
+// code is inspired from this article
+// https://itnext.io/flutter-handling-your-network-api-calls-like-a-boss-936eef296547
+
+final logger = getLogger();
+
+class ApiProvider {
+  Future<dynamic> get(String apiEndPoint) async {
+    //logging
+    logger.d('API Provider - GET request: ${apiEndPoint}');
+
+    var responseJson;
+    try {
+      final response = await http.get(apiEndPoint);
+      responseJson = _response(response);
+    } on SocketException {
+      throw FetchDataException('No Internet connection');
+    }
+//    log.i('API Provider - Response: ${responseJson}');
+    return responseJson;
+  }
+
+  dynamic _response(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        var responseJson = json.decode(response.body.toString());
+        // print(responseJson);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 401:
+
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+
+      default:
+        throw FetchDataException(
+            'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+    }
+  }
+}
